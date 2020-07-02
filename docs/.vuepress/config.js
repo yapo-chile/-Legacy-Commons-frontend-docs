4584178a424f7ce20912f41443895bac82383259
Fernanda Cancino <fernandacancino@adevinta.com>
2020-07-02 11:17:51 -0400

    feat/doc-spanish-version: first doc commit in spanish

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,39 +3,115 @@ module.exports = {
   description: 'frontend docs',
   themeConfig: {
     logo: 'https://static.yapo.cl/shared/icons/logo.svg',
-    nav: [
-      {
-        text: 'Home',
-        link: '/'
-      },
-      {
-        text: 'Get started',
-        link: '/basic/'
-      },
-      {
-        text: 'Links',
-        items: [
+    locales: {
+      '/': {
+        nav: [
           {
-            text: 'docs repo',
-            link: 'https://github.mpi-internal.com/Yapo/frontend-docs',
-            target:'_blank'
+            text: 'Home',
+            link: '/'
           },
           {
-            text: 'docs vuepress',
-            link: 'https://vuepress.vuejs.org/',
-            target:'_blank'
-          }
-        ]
+            text: 'Get started',
+            link: '/basic/'
+          },
+          {
+            text: 'Links',
+            items: [
+              {
+                text: 'docs repo',
+                link: 'https://github.mpi-internal.com/Yapo/frontend-docs',
+                target:'_blank'
+              },
+              {
+                text: 'docs vuepress',
+                link: 'https://vuepress.vuejs.org/',
+                target:'_blank'
+              }
+            ]
+          },
+        ],
+        sidebar: [
+          '/basic/',
+          '/basic/team'
+        ],
       },
-    ],
-    sidebar: [
-      '/basic/',
-      '/basic/team'
-    ],
+      '/es/': {
+        nav: [
+          {
+            text: 'Inicio',
+            link: '/'
+          },
+          {
+            text: 'Empezar',
+            link: 'basic/'
+          },
+          {
+            text: 'Links',
+            items: [
+              {
+                text: 'docs repo',
+                link: 'https://github.mpi-internal.com/Yapo/frontend-docs',
+                target:'_blank'
+              },
+              {
+                text: 'docs vuepress',
+                link: 'https://vuepress.vuejs.org/',
+                target:'_blank'
+              }
+            ]
+          },
+        ],
+        sidebar: [
+          '/basic/',
+          '/basic/team'
+        ],
+      },
+    },
+    // nav: [
+    //   {
+    //     text: 'Home',
+    //     link: '/'
+    //   },
+    //   {
+    //     text: 'Get started',
+    //     link: '/basic/'
+    //   },
+    //   {
+    //     text: 'Links',
+    //     items: [
+    //       {
+    //         text: 'docs repo',
+    //         link: 'https://github.mpi-internal.com/Yapo/frontend-docs',
+    //         target:'_blank'
+    //       },
+    //       {
+    //         text: 'docs vuepress',
+    //         link: 'https://vuepress.vuejs.org/',
+    //         target:'_blank'
+    //       }
+    //     ]
+    //   },
+    // ],
+    // sidebar: [
+    //   '/basic/',
+    //   '/basic/team'
+    // ],
     head: [
       ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon.png"}],
       ['link', { rel: "shortcut icon", href: "/favicon.ico"}],
     ],
     plugins: ['@vuepress/active-header-links']
-  }
+  },
+  locales: {
+    '/': {
+      lang: 'en-US',
+      title: 'VuePress',
+      description: 'Vue En',
+    },
+    '/es/': {
+      lang: 'es-CH',
+      title: 'VuePress',
+      description: 'Vue Es',
+    }
+  },
 }
---
+++ docs/es/basic/README.md
@@ -0,0 +1,65 @@
+# Empezando 
+
+Hay varias cosas que necesitas instalar y configurar antes de empezar.
+
+## Instalar [Homebrew](https://brew.sh/)
+``` bash
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+```
+
+## Instalar [Git](https://git-scm.com/)
+``` bash
+brew install git
+```
+
+## Instalar [NodeJS](https://nodejs.org/)
+``` bash
+brew install node
+```
+
+## Instalar [Yarn](https://yarnpkg.com/)
+``` bash
+brew install yarn
+```
+
+## Instalar [Docker](https://www.docker.com/)
+``` bash
+brew cask install docker
+```
+
+## Instalar [TunnelBlick](https://tunnelblick.net/)
+``` bash
+brew cask install tunnelblick
+```
+
+## Instalar [VScode](https://code.visualstudio.com/)
+``` bash
+brew cask install visual-studio-code
+```
+
+## Configurar Git con tu nombre e email
+``` bash
+git config global user.name "First Last" (keep the quotes)
+git config global user.email "Email" (keep the quotes)
+```
+
+## Generar una clave SSH 
+``` bash
+ssh-keygen -t rsa -b 4096 -C <yourname@adevinta.com>
+ssh-add ~/.ssh/id_rsa
+```
+
+## Clonar [ADV cli](https://github.mpi-internal.com/cli/adv/)
+``` bash
+git clone git@github.mpi-internal.com:cli/adv.git
+cd adv
+./bin/adv-install
+source "/Users/<user>/.adv/bin/adv-shell-integration-v1.sh"
+```
+
+## Agregar archivos hosts
+``` bash
+sudo vim /etc/hosts
+127.0.0.1 regress.yapo.cl
+172.21.10.101 registry01.msf.yapo.cl
+```
